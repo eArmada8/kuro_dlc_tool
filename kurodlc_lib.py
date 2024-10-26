43645643f7663136b04264399abe3599c706fc15
eArmada8 <103985972+eArmada8@users.noreply.github.com>
2024-10-26 16:31:45 -0700

    Multilanguage support in one p3a (Ys X)

--- kurodlc_lib.py
+++ kurodlc_lib.py
@@ -20,7 +20,8 @@ class kuro_tables:
         self.init_schemas()
 
     def init_schemas(self):
-        tables = glob.glob('*.tbl.original') + [x for x in glob.glob('*.tbl') if not os.path.exists(x+'.original')]
+        tables = glob.glob('**/*.tbl.original', recursive = True) \
+            + [x for x in glob.glob('**/*.tbl', recursive = True) if not os.path.exists(x+'.original')]
         for table_name in tables:
             with open(table_name, 'rb') as f:
                 magic = f.read(4)
