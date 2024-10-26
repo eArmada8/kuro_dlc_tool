c0d4f4cffa62db39f7ab7fb53c02660e3c0fa878
eArmada8 <103985972+eArmada8@users.noreply.github.com>
2024-10-26 15:16:43 -0700

    Code simplification

--- kurodlc_make_json_from_tbls.py
+++ kurodlc_make_json_from_tbls.py
@@ -24,32 +24,23 @@ if __name__ == "__main__":
         os.chdir(os.path.abspath(os.path.dirname(__file__)))
 
     kt = kuro_tables()
-    
-    if all([os.path.exists(x) for x in ['t_costume.tbl', 't_costume.tbl.original',\
-            't_dlc.tbl', 't_dlc.tbl.original', 't_item.tbl', 't_item.tbl.original']]):
-        t_costume_original = kt.read_table('t_costume.tbl.original')
-        t_dlc_original = kt.read_table('t_dlc.tbl.original')
-        t_item_original = kt.read_table('t_item.tbl.original')
-        t_costume = kt.read_table('t_costume.tbl')
-        t_dlc = kt.read_table('t_dlc.tbl')
-        t_item = kt.read_table('t_item.tbl')
-        kurodlc_json = {\
-            'CostumeParam': [x for x in t_costume['CostumeParam']\
-                if x not in t_costume_original['CostumeParam']],\
-            'DLCTableData': [x for x in t_dlc['DLCTableData']\
-                if x['id'] not in [x['id'] for x in t_dlc_original['DLCTableData']]],\
-            'ItemTableData': [x for x in t_item['ItemTableData']\
-                if x['id'] not in [x['id'] for x in t_item_original['ItemTableData']]]}
-        if all([os.path.exists(x) for x in ['t_shop.tbl', 't_shop.tbl.original']]):
-            t_shop_original = kt.read_table('t_shop.tbl.original')
-            t_shop = kt.read_table('t_shop.tbl')
-            kurodlc_json['ShopItem'] = [x for x in t_shop['ShopItem']\
-                if x not in t_shop_original['ShopItem']]
+
+    kurodlc_json = {}
+    for table in ['t_costume', 't_dlc', 't_item', 't_recipe', 't_shop']:
+        new_tbl, orig_tbl = table + '.tbl', table + '.tbl.original'
+        if os.path.exists(new_tbl) and os.path.exists(orig_tbl):
+            orig_tbl_data = kt.read_table(orig_tbl)
+            new_tbl_data = kt.read_table(new_tbl)
+            for subtable in orig_tbl_data:
+                diff_entries = [x for x in new_tbl_data[subtable]
+                    if not x in orig_tbl_data[subtable]]
+                if len(diff_entries) > 0:
+                    kurodlc_json[subtable] = diff_entries
+    if len(kurodlc_json) > 0:
         json_name = input("Please input name for .kurodlc.json file (e.g. \"my_mod\" for \"my_mod.kurodlc.json\") ")
         valid = '-_.[]() abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
         json_name = ''.join([x if x in valid else '_' for x in json_name])
         kt.write_struct_to_json(kurodlc_json, json_name + '.kurodlc.json')
     else:
-        print("The following files are missing! {}".format([x for x in ['t_costume.tbl', 't_costume.tbl.original',\
-            't_dlc.tbl', 't_dlc.tbl.original', 't_item.tbl', 't_item.tbl.original'] if not os.path.exists(x)]))
+        print("No entries found, all analyzed tbl files equal or unreadable (or originals not available.")
         input("Press Enter to quit.")
