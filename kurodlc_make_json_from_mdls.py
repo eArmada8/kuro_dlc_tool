4c36c59cbb2b5783da9239e38f527b8ff360fb59
eArmada8 <103985972+eArmada8@users.noreply.github.com>
2025-01-29 14:38:13 -0800

    Kai t_name compatibility
    Workaround needed because t_name.tbl in Kai replaces basenames with the new costume versions (e.g. chr5001 has been replaced with chr5001_c15).  Also see commit [0a17adb](https://github.com/eArmada8/kuro_dlc_tool/commit/0a17adb500c877bef53db12c3dc6048a98b52107)

--- kurodlc_make_json_from_mdls.py
+++ kurodlc_make_json_from_mdls.py
@@ -114,7 +114,9 @@ class dlc_table_maker:
                 base_name = "_".join(mdl_name.split('.')[0].split("_")[0:1])
                 if base_name[:2] == 'fc':
                     base_name = base_name[1:]
-                if base_name + '_c10' in self.name_dict and not base_name in self.name_dict: # Kuro 2
+                if base_name + '_c15' in self.name_dict and not base_name in self.name_dict: # Kai
+                    base_name = base_name + '_c15'
+                elif base_name + '_c10' in self.name_dict and not base_name in self.name_dict: # Kuro 2
                     base_name = base_name + '_c10'
             elif self.game_type == 'ys_x':
                 mdl_prefix = mdl_name.split('.')[0]
