b7113abc7ca000e5fe3a69fcb347127b007275bb
eArmada8 <103985972+eArmada8@users.noreply.github.com>
2025-05-17 12:16:55 -0700

    Move tbl schemas into separate file

--- kurodlc_lib.py
+++ kurodlc_lib.py
@@ -12,6 +12,7 @@ except ModuleNotFoundError as e:
 
 class kuro_tables:
     def __init__(self):
+        self.schemas = {}
         self.schema_dict = {}
         self.crc_dict = {}
         self.data2_start_offset = 0
@@ -21,6 +22,12 @@ class kuro_tables:
         self.init_schemas()
 
     def init_schemas(self):
+        if os.path.exists('kurodlc_schema.json'):
+            kurodlc_schema = json.loads(open('kurodlc_schema.json','rb').read())
+            self.schemas = {(x['table_header'],x['schema_length']):x['schema'] for x in kurodlc_schema}
+        else:
+            print("kurodlc_schema.json is missing!  This tool will not be able to read tables.")
+            input("Press Enter to continue.")
         tables = glob.glob('**/*.tbl.original', recursive = True) \
             + [x for x in glob.glob('**/*.tbl', recursive = True) if not os.path.exists(x+'.original')]
         for table_name in tables:
@@ -40,158 +47,8 @@ class kuro_tables:
 
     def get_schema(self, name, entry_length):
         self.schema_dict[name] = entry_length
-        if name == 'DLCTableData' and entry_length == 88: # Kuro 1
-            self.schema_dict[name] = entry_length
-            return({'schema': "<2IQ2IQ2I3Q2HIQ2I", 'sch_len': 88,\
-                'keys': ['id', 'sort_id', 'items', 'unk0', 'quantity',\
-                    'unk1', 'name', 'desc', 'unk_txt', 'unk2', 'unk3',\
-                    'unk4', 'unk_arr', 'unk5'],\
-                'values': 'nnanantttnnnan', 'primary_key': 'id'})
-        elif name == 'DLCTableData' and entry_length == 64: # Kuro 2
-            self.schema_dict[name] = entry_length
-            return({'schema': "<2IQ2IQ2I3Q", 'sch_len': 64,\
-                'keys': ['id', 'sort_id', 'items', 'unk0', 'quantity',\
-                    'unk1', 'name', 'desc', 'unk_txt'],\
-                'values': 'nnananttt', 'primary_key': 'id'})
-        elif name == 'DLCTable' and entry_length == 64: # Ys X
-            self.schema_dict[name] = entry_length
-            return({'schema': "<2IQ2IQ2I3Q", 'sch_len': 64,\
-                'keys': ['id', 'sort_id', 'items', 'unk0', 'quantity',\
-                    'unk1', 'name', 'type_desc', 'desc'],\
-                'values': 'nnananttt', 'primary_key': 'id'})
-        elif name == 'ItemTableData' and entry_length == 248: # Kuro 1 / 2
-            return({'schema': "<2I2Q2BIHI2f20If20I3Q4I", 'sch_len': 248,\
-                'keys': ['id','chr_restrict','flags','unk_txt','category','subcategory',\
-                    'unk0','unk1','unk2','unk3','unk4','eff1_id','eff1_0','eff1_1','eff1_2',\
-                    'eff2_id','eff2_0','eff2_1','eff2_2','eff3_id','eff3_0','eff3_1','eff3_2',\
-                    'eff4_id','eff4_0','eff4_1','eff4_2','eff5_id','eff5_0','eff5_1','eff5_2',\
-                    'unk5','hp','ep','patk','pdef','matk','mdef','str','def','ats','adf','agl',\
-                    'dex','hit','eva','meva','crit','spd','mov','stack_size','price','anim',\
-                    'name','desc','unk6','unk7','unk8','unk9'],\
-                'values': 'nnttnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnntttnnnn',\
-                'primary_key': 'id'})
-        elif name == 'ItemTableData' and entry_length == 176: # Ys X
-            return({'schema': "<2I3QI2hI3I4BI2B3HQfI10iIfIfIfIfQ2I", 'sch_len': 176,\
-                'keys': ['id','icon','name','short_desc','desc','unk0','type','unk1','unk2',\
-                'unused0','unused1','unused2',\
-                'unk3','unk4','unk5','unk6','unk7','unk8','unk9','unused3','unused4','unused5',\
-                'obtain_type','obtain_amount','unk10', 'hp','str','break',\
-                'def','vit','luck','crit','eva','dmg','dmg_received',\
-                'eff1_id','eff1_0','eff2_id','eff2_0','eff3_id','eff3_0','eff4_id','eff4_0',\
-                'unk_txt','unk11','unk12'],\
-                'values': 'nntttnnnnnnnnnnnnnnnnntnnnnnnnnnnnnnnnnnnnntnn',\
-                'primary_key': 'id'})
-        elif name == 'ItemKindParam2' and entry_length == 16: # Kuro 1 / 2
-            return({'schema': "<2Q", 'sch_len': 16,\
-                'keys': ['id', 'value'],\
-                'values': 'nt', 'primary_key': 'id'})
-        elif name == 'QuartzParam' and entry_length == 28: # Kuro 1 / 2
-            return({'schema': "<8H8BI", 'sch_len': 28,\
-                'keys': ['id', 'cost_e', 'cost_wa', 'cost_f', 'cost_wi', 'cost_t', 'cost_s', 'cost_m',\
-                    'quant_e', 'quant_wa', 'quant_f', 'quant_wi', 'quant_t', 'quant_s', 'quant_m', 'unk0', 'unk1'],\
-                'values': 'nnnnnnnnnnnnnnnnn', 'primary_key': 'id'})
-        elif name == 'ItemTabType' and entry_length == 12: # Kuro 1 / 2
-            return({'schema': "<3I", 'sch_len': 12,\
-                'keys': ['id', 'int1', 'int2'],\
-                'values': 'nnn', 'primary_key': 'id'})
-        elif name == 'CostumeParam' and entry_length == 56: # Kuro 1 / 2
-            return({'schema': "<4H2Q2I3Q", 'sch_len': 56,\
-                'keys': ['char_restrict', 'type', 'item_id', 'unk0', 'unk_txt0', 'mdl_name',\
-                    'unk1', 'unk2', 'attach_name', 'unk_txt1', 'unk_txt2'],\
-                'values': 'nnnnttnnttt'})
-        elif name == 'CostumeAttachOffset' and entry_length == 56: # Kuro 1 / 2
-            return({'schema': "<2IQ10f", 'sch_len': 56,\
-                'keys': ['char_id', 'unk0', 'mdl_name', 't0', 't1', 't2', 'r0', 'r1', 'r2',\
-                    's0', 's1', 's2', 'unk1'],\
-                'values': 'nntnnnnnnnnnn'})
-        elif name == 'CostumeTable' and entry_length == 24: # Ys X
-            return({'schema': "<2I2Q", 'sch_len': 24,\
-                'keys': ['character_id', 'item_id', 'base_model', 'costume_model'],\
-                'values': 'nntt', 'primary_key': 'item_id', 'unique_values': ['costume_model']})
-        elif name == 'CostumeAttachTable' and entry_length == 80: # Ys X
-            return({'schema': "<2I2i8Q", 'sch_len': 80,\
-                'keys': ['character_id', 'unk0', 'item_id', 'unk1', 'base_model',\
-                'equip_model', 'attach_point', 'unk_text0', 'unk_text1',
-                'unk_text2', 'unk_text3', 'unk_text4'],\
-                'values': 'nnnntttttttt', 'primary_key': 'item_id', 'unique_values': ['equip_model']})
-        elif name == 'CostumeMaterialTable' and entry_length == 24: # Ys X
-            return({'schema': "<2I2Q", 'sch_len': 24,\
-                'keys': ['character_id', 'item_id', 'base_model', 'equip_model'],\
-                'values': 'nntt', 'primary_key': 'item_id', 'unique_values': ['equip_model']})
-        elif name == 'CostumeUIFaceTable' and entry_length == 48: # Ys X
-            return({'schema': "<Ii2IQ2IQ2I", 'sch_len': 48,\
-                'keys': ['character_id', 'unk0', 'unk1', 'unk2', 'unk_array0',\
-                'unk3', 'unk_array1', 'unk4'],\
-                'values': 'nnnnanan'})
-        elif name == 'NameTableData' and entry_length == 88: # Kuro 1
-            return({'schema': "<11Q", 'sch_len': 88,\
-                'keys': ['char_id', 'name', 'model', 'face', 'texture',\
-                    'unk0', 'unk_txt0', 'unk_txt1', 'unk1', 'unk_txt2', 'unk_txt3'],\
-                'values': 'nttttnttntt'})
-        elif name == 'NameTableData' and entry_length == 104: # Kuro 2
-            return({'schema': "<13Q", 'sch_len': 104,\
-                'keys': ['char_id', 'name', 'model', 'face', 'texture',\
-                    'unk0', 'unk_txt0', 'unk1', 'unk_txt1', 'unk2', 'unk_txt2',\
-                    'unk_txt3', 'unk_txt4'],\
-                'values': 'nttttntntnttt'})
-        elif name == 'SNameTable' and entry_length == 96: # Ys X
-            return({'schema': "<2I9QiIQ", 'sch_len': 96,\
-                'keys': ['char_id', 'unk0', 'name', 'unk1', 'unk2',\
-                    'model', 'ani', 'face', 'unk_model0', 'unk_model1', 'unk_model2',\
-                    'unk3', 'unk4', 'unk_txt'],\
-                'values': 'nntnnttttttnnt'})
-        elif name == 'ShopInfo' and entry_length == 80: # Kuro 1 / 2
-            return({'schema': "<4Q2H7f4I", 'sch_len': 80,\
-                'keys': ['id', 'shop_name', 'long1', 'flag', 'empty', 'short1',\
-                    'float1', 'float2', 'float3', 'float4', 'float5', 'float6',\
-                    'float7', 'int1', 'int2', 'int3', 'int4'],\
-                'values': 'ntntnnnnnnnnnnnnn'})
-        elif name == 'ShopItem' and entry_length == 40: # Kuro 1 / 2
-            return({'schema': "<2HIQ2IQ2I", 'sch_len': 40,\
-                'keys': ['shop_id', 'item_id', 'unknown', 'start_scena_flags',\
-                    'empty1', 'end_scena_flags', 'int2'],\
-                'values': 'nnnbnbn'})
-        elif name == 'ShopTypeDesc' and entry_length == 24: # Kuro 1 / 2
-            return({'schema': "<2Q8B", 'sch_len': 24,\
-                'keys': ['id', 'flag', 'byte1', 'byte2', 'byte3', 'byte4', 'byte5',\
-                    'byte6', 'byte7', 'byte8'],\
-                'values': 'ntnnnnnnnn'})
-        elif name == 'ShopConv' and entry_length == 36: # Kuro 1 / 2
-            return({'schema': "<I8f", 'sch_len': 36,\
-                'keys': ['id', 'float1', 'float2', 'float3', 'float4', 'float5',\
-                    'float6', 'float7', 'float8'],\
-                'values': 'nnnnnnnnn'})
-        elif name == 'TradeItem' and entry_length == 52: # Kuro 1 / 2
-            return({'schema': "<13I", 'sch_len': 52,\
-                'keys': ['item_id', 'trade_item_id1', 'quant1', 'trade_item_id2',\
-                    'quant2', 'trade_item_id3', 'quant3', 'trade_item_id4', 'quant4',\
-                    'trade_item_id5', 'quant5', 'trade_item_id6', 'quant6'],\
-                'values': 'nnnnnnnnnnnnn'})
-        elif name == 'ShopInfo' and entry_length == 32: # Ys X
-            return({'schema': "<2I3Q", 'sch_len': 32,\
-                'keys': ['id', 'unk0', 'shop_name', 'unk_txt1', 'unk_txt2'],\
-                'values': 'nnttt'})
-        elif name == 'ProductInfo' and entry_length == 96: # Ys X
-            return({'schema': "<2I2iQ2IQ2IQ2I4i2Q", 'sch_len': 96,\
-                'keys': ['recipe_id', 'shop_id', 'max_quantity', 'int2', 'arr0',\
-                'int3', 'arr1', 'int4', 'arr2', 'int5', 'int6', 'int7', 'int8',\
-                'int9', 'unk_txt0', 'unk_txt1'],\
-                'values': 'nnnnananannnnntt'})
-        elif name == 'ShopNameInfo' and entry_length == 24: # Ys X
-            return({'schema': "<2I2Q", 'sch_len': 24,\
-                'keys': ['unk0', 'unk1', 'unk_txt0', 'unk_txt1'],\
-                'values': 'nntt'})
-        elif name == 'RecipeTableData' and entry_length == 60: # Ys X
-            return({'schema': "<15I", 'sch_len': 60,\
-                'keys': ['recipe_id', 'unk0', 'item_id', 'mat1_id', 'mat1_quant',\
-                'mat2_id', 'mat2_quant', 'mat3_id', 'mat3_quant', 'mat4_id', 'mat4_quant',\
-                'mat5_id', 'mat5_quant', 'mat6_id', 'mat6_quant'],\
-                'values': 'nnnnnnnnnnnnnnn', 'primary_key': 'recipe_id'})
-        elif name == 'BargainItem' and entry_length == 48: # Kai
-            return({'schema': "<I2H2Q2IQ2I", 'sch_len': 48,\
-                'keys': ['unk0', 'unk1', 'unk2', 'unk3',\
-                'unk_arr0', 'unk4', 'unk_arr1', 'unk5'],\
-                'values': 'nnnnbnbn'})
+        if (name, entry_length) in self.schemas:
+            return self.schemas[(name, entry_length)]
         else:
             return({})
 
@@ -218,39 +75,43 @@ class kuro_tables:
         for key in [x for x in json_data if len(json_data[x]) > 0]:
             if key in self.schema_dict:
                 schema = self.get_schema(key, self.schema_dict[key])
-                if not all([list(json_data[key][i].keys()) == schema['keys']\
-                        for i in range(len(json_data[key]))]):
-                    if [all([x in schema['keys'] for x in list(json_data[key][i].keys())]) for i in range(len(json_data[key]))]:
-                        for i in range(len(json_data[key])):
-                            if not list(json_data[key][i].keys()) == schema['keys']:
-                                json_data[key][i] = {x:json_data[key][i][x] for x in schema['keys']}
-                    else:
-                        print("Validation of {0} failed, schema in {1} is incorrect!".format(json_name, key))
-                        input("Invalid or missing keys, auto-correction not possible.")
+                if len(schema) > 0:
+                    if not all([list(json_data[key][i].keys()) == schema['keys']\
+                            for i in range(len(json_data[key]))]):
+                        if [all([x in schema['keys'] for x in list(json_data[key][i].keys())]) for i in range(len(json_data[key]))]:
+                            for i in range(len(json_data[key])):
+                                if not list(json_data[key][i].keys()) == schema['keys']:
+                                    json_data[key][i] = {x:json_data[key][i][x] for x in schema['keys']}
+                        else:
+                            print("Validation of {0} failed, schema in {1} is incorrect!".format(json_name, key))
+                            input("Invalid or missing keys, auto-correction not possible.")
+                            raise
+                    pass_value_validation = True
+                    problem_keys = []
+                    for i in range(len(schema['values'])):
+                        if schema['values'][i] == 'n':
+                            if not all([isinstance(json_data[key][j][schema['keys'][i]], int)\
+                                    or isinstance(json_data[key][j][schema['keys'][i]], float) for j in range(len(json_data[key]))]):
+                                problem_keys.append(schema['keys'][i])
+                                pass_value_validation = False
+                        elif schema['values'][i] in ['a', 'b']:
+                            if not all([isinstance(json_data[key][j][schema['keys'][i]], list)\
+                                    and all([isinstance(k, int) for k in json_data[key][j][schema['keys'][i]]])\
+                                    for j in range(len(json_data[key]))]):
+                                problem_keys.append(schema['keys'][i])
+                                pass_value_validation = False
+                        elif schema['values'][i] == 't':
+                            if not all([isinstance(json_data[key][j][schema['keys'][i]], str) for j in range(len(json_data[key]))]):
+                                problem_keys.append(schema['keys'][i])
+                                pass_value_validation = False
+                    if pass_value_validation == False:
+                        input("Validation of {0} failed, values {1} in {2} do not match the schema!".format(json_name, problem_keys, key))
                         raise
-                pass_value_validation = True
-                problem_keys = []
-                for i in range(len(schema['values'])):
-                    if schema['values'][i] == 'n':
-                        if not all([isinstance(json_data[key][j][schema['keys'][i]], int)\
-                                or isinstance(json_data[key][j][schema['keys'][i]], float) for j in range(len(json_data[key]))]):
-                            problem_keys.append(schema['keys'][i])
-                            pass_value_validation = False
-                    elif schema['values'][i] in ['a', 'b']:
-                        if not all([isinstance(json_data[key][j][schema['keys'][i]], list)\
-                                and all([isinstance(k, int) for k in json_data[key][j][schema['keys'][i]]])\
-                                for j in range(len(json_data[key]))]):
-                            problem_keys.append(schema['keys'][i])
-                            pass_value_validation = False
-                    elif schema['values'][i] == 't':
-                        if not all([isinstance(json_data[key][j][schema['keys'][i]], str) for j in range(len(json_data[key]))]):
-                            problem_keys.append(schema['keys'][i])
-                            pass_value_validation = False
-                if pass_value_validation == False:
-                    input("Validation of {0} failed, values {1} in {2} do not match the schema!".format(json_name, problem_keys, key))
+                else:
+                    input("Validation of {0} in {1} skipped, schema not found! (tbl file not supported)".format(key, json_name))
                     raise
             else:
-                input("Validation of {0} in {1} failed, schema not found! (tbl file missing)".format(key, json_name))
+                input("Validation of {0} in {1} failed, schema not found! (tbl file is missing)".format(key, json_name))
                 raise
         return(json_data)
 
